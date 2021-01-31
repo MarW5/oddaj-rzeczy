bbb0ed98ccb2863f81b50ddd2aeae63e3f98dff4
MarW5 <marta.wiese5@gmail.com>
2021-01-31 21:55:19 +0100

    Mobile menu, responsive views things giving, log, reg screen

--- src/components/Home_components/HomeHeader.js
+++ src/components/Home_components/HomeHeader.js
@@ -3,7 +3,7 @@ import { Link as ScrollLink} from 'react-scroll'
 import {Link, NavLink, } from 'react-router-dom';
 import { isMobile } from 'react-device-detect'
 import menuHamburger from '../../assets/assets/menu.svg'
-
+import crossMenu from '../../assets/assets/cross_menu.svg'
 
 class HomeHeader extends Component{
       constructor(props){
@@ -17,25 +17,32 @@ class HomeHeader extends Component{
                   mobile: !this.state.mobile
             })
       }
+
+      clickLink = e =>{
+            this.setState({
+                  mobile: !this.state.mobile
+            })
+      }
+      
       render(){
             console.log(!this.state.mobile)
-            const activeStyle = {
+            const activeStyle= {
                   border:'1px solid $color4'
             };
             if(isMobile){
                   return(
                         <div className="Header_menu">
-                              <span className ="hamburger_menu" onClick = {this.hamburgerMenu}><img src= {menuHamburger} alt="menu_hamburger"/></span>
+                             {!this.state.mobile ? <span className ="hamburger_menu" onClick = {this.hamburgerMenu}><img src= {menuHamburger} alt="menu_hamburger"/></span> :  <span className="cross_menu" onClick= {this.hamburgerMenu}><img src= {crossMenu} alt="menu_cross"/></span>} 
                               <ul className="Header_logg_option">
                                     <li><Link to="/logowanie">Zaloguj</Link></li>
                                     <li><Link to="/rejestracja">Załóż konto</Link></li>
                               </ul>
                               {this.state.mobile ? <ul className=" Header_menu_nav mobile_menu">
-                                    <li className="menu_nav_link"><NavLink exact to="/" activeStyle={activeStyle} className="nav_link_menu">Start</NavLink></li>
-                                    <li className="menu_nav_link"><ScrollLink activeClass="active" to="FourSteps" spy={true} smooth={true} duration={500} className="nav_link_menu">O co chodzi?</ScrollLink></li>
-                                    <li className="menu_nav_link"><ScrollLink activeClass="active" to="AboutUs" spy={true} smooth={true} duration={500} className="nav_link_menu">O nas</ScrollLink></li>
-                                    <li className="menu_nav_link"><ScrollLink activeClass="active" to="HelpPeople" spy={true} smooth={true} duration={500} className="nav_link_menu">Fundacja i organizacje</ScrollLink></li>
-                                    <li className="menu_nav_link"><ScrollLink activeClass="active" to="Contact" spy={true} smooth={true} duration={500} className="nav_link_menu">Kontakt</ScrollLink></li>                  
+                                    <li onClick= {this.clickLink} className="menu_nav_link"><NavLink exact to="/" activeStyle={activeStyle} className="nav_link_menu">Start</NavLink></li>
+                                    <li className="menu_nav_link"><ScrollLink activeClass="active" onClick= {this.clickLink} to="FourSteps" spy={true} smooth={true} duration={500} className="nav_link_menu">O co chodzi?</ScrollLink></li>
+                                    <li className="menu_nav_link"><ScrollLink activeClass="active" onClick= {this.clickLink} to="AboutUs" spy={true} smooth={true} duration={500} className="nav_link_menu">O nas</ScrollLink></li>
+                                    <li className="menu_nav_link"><ScrollLink activeClass="active" onClick= {this.clickLink} to="HelpPeople" spy={true} smooth={true} duration={500} className="nav_link_menu">Fundacja i organizacje</ScrollLink></li>
+                                    <li className="menu_nav_link"><ScrollLink activeClass="active" onClick= {this.clickLink} to="Contact" spy={true} smooth={true} duration={500} className="nav_link_menu">Kontakt</ScrollLink></li>                  
                               </ul> : null}
                         </div>
                   )
